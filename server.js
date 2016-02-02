1209c89ce758e0fb796172a12d509250e4bd3689
ceckenrode <chriseckenrode@me.com>
2016-02-01 20:44:29 -0500

    made code more dry by using just one function for all page requests

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ var handleRequest = function(req, res) {
   var url_parts = url.parse(req.url);
   switch (url_parts.pathname) {
     case '/':
-      display_root(req, res);
+      display_page("home.html", req, res);
       break;
     case '/home':
-      display_root(req, res);
+      display_page("home.html", req, res);
       break;
     case '/favorite_food':
-      display_food(req, res);
+      display_page("favorite_food.html", req, res);
       break;
     case '/css':
-      display_css(req, res);
+      display_page("css.html", req, res);
       break;
     case '/favorite_movies':
-      display_movies(req, res);
+      display_page("favorite_movies.html", req, res);
       break;
     default:
-      display_404(req, res);
+      display_page("404.html", req, res);
       break;
   };
 };
@@ -33,56 +33,8 @@ server.listen(PORT, function() {
   console.log("Server is listening at http://localhost:%s", PORT);
 });
 
-var display_root = function(req, res) {
-  fs.readFile('home.html', function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
-  });
-}
-
-var display_food = function(req, res) {
-  fs.readFile('favorite_food.html', function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
-  });
-}
-
-var display_css = function(req, res) {
-  fs.readFile('css.html', function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
-  });
-}
-
-var display_movies = function(req, res) {
-  fs.readFile('favorite_movies.html', function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
-  });
-}
-
-var display_404 = function(req, res) {
-  fs.readFile('404.html', function(err, data) {
+var display_page = function(page, req, res) {
+  fs.readFile(page, function(err, data) {
     if (err) {
       return console.error(err);
     }
